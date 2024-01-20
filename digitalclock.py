38421721781c3b268766377983251e5fc64c0038
Eurin HASH <eurindelagoldcoast@gmail.com>
2024-01-20 16:21:31 +0100

    update
    Les principales corrections apportées sont les suivantes :
    
        Correction de la chaîne de formatage dans strftime("%H:%M:%S").
        Renommage de label1 à label2 pour éviter la redéfinition de la variable.
        Ajout d'une étiquette manquante (label6) dans la fonction labels.

--- digitalclock.py
+++ digitalclock.py
@@ -2,54 +2,51 @@ from tkinter import *
 from datetime import datetime
 from time import strftime
 
-WIN =  Tk()
+WIN = Tk()
 WIN.maxsize(480, 120)
 WIN.resizable(False, False)
 WIN.configure(bg="black")
-WIN.title("DIGITAL CLOCK") 
+WIN.title("DIGITAL CLOCK")
 
 def stay_on_top():
-   WIN.lift()
-   WIN.after(2000, stay_on_top)
+    WIN.lift()
+    WIN.after(2000, stay_on_top)
 
 a = datetime.today().strftime("%A")
 b = (a.upper())
-c = (b[0:3]) 
+c = (b[0:3])
 
-frame =  Frame(WIN, width=750, height=200, bg="black")
+frame = Frame(WIN, width=750, height=200, bg="black")
 frame.pack(expand=True)
 
 def time():
-
-    a = strftime("%H : %M : %S")
-    label.config(text= a)
+    a = strftime("%H:%M:%S")
+    label.config(text=a)
     label.after(1000, time)
 
 label = Label(frame, font=("Century Gothic", 50), bg="black", fg="white")
-
-label.place(x= 32, y=15)
+label.place(x=32, y=15)
 time()
 
-label1 = Label(frame, font=('Century Gothic',50), bg='black', fg='white')
+label1 = Label(frame, font=('Century Gothic', 50), bg='black', fg='white')
 label1.config(text=c)
 label1.place(x=358, y=15)
 
-label1 = Label(frame, font=('Century Gothic',50), bg='black', fg='white', text='|')
-label1.place(x=300, y=10)
+label2 = Label(frame, font=('Century Gothic', 50), bg='black', fg='white', text='|')
+label2.place(x=300, y=10)
 
 def labels():
+    label3 = Label(frame, font=('Century Gothic', 10), bg='#0e1013', fg='#7f7f7f', text='DAY')
+    label3.place(x=392, y=80)
 
-    label2=Label(frame, font=('Century Gothic',10),bg='#0e1013',fg='#7f7f7f',text='DAY')
-    label2.place(x=392, y=80)
+    label4 = Label(frame, font=('Century Gothic', 10), bg='#0e1013', fg='#7f7f7f', text='HOURS')
+    label4.place(x=46, y=80)
 
-    label3=Label(frame, font=('Century Gothic',10),bg='#0e1013',fg='#7f7f7f',text='HOURS')
-    label3.place(x=46, y=80)
+    label5 = Label(frame, font=('Century Gothic', 10), bg='#0e1013', fg='#7f7f7f', text='MINUTES')
+    label5.place(x=134, y=80)
 
-    label4=Label(frame, font=('Century Gothic',10),bg='#0e1013',fg='#7f7f7f',text='MINUTES')
-    label4.place(x=134, y=80)
- 
-    label5=Label(frame, font=('Century Gothic',10),bg='#0e1013',fg='#7f7f7f',text='SECONDS')
-    label5.place(x=230, y=80)
+    label6 = Label(frame, font=('Century Gothic', 10), bg='#0e1013', fg='#7f7f7f', text='SECONDS')
+    label6.place(x=230, y=80)
 
 labels()
 
